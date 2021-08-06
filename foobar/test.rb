9f6e41b66d3d30f0bbaac39e717727e7e9302142
Duyet Nguyen <duyet.vn@gmail.com>
2021-08-06 23:17:18 +0000

    update test

--- foobar/test.rb
+++ foobar/test.rb
@@ -4,6 +4,10 @@ require_relative './main'
 
 describe '"FooBar Test"' do
   it 'From 1 to 10' do
-    expect(foobar(10)).to eql('1 2 bar 4 foo bar 7 8 bar foo')
+    expect(foobar(10)).to eql('1 2 foo 4 bar foo 7 8 foo bar')
+  end
+
+  it 'From 1 to 20' do
+    expect(foobar(20)).to eql('1 2 foo 4 bar foo 7 8 foo bar 11 foo 13 14 foobar 16 17 foo 19 bar')
   end
 end
