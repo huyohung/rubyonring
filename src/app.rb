11f7c258761a28eafdbdf4d87ae04247053b0ad5
Huy Hung <hung.bui1@ntq-solution.com.vn>
2021-08-11 22:11:04 +0700

    resolve foobar function

--- src/app.rb
+++ src/app.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
-def foobar(number) end
+def foobar(number)
+  (1..number).to_a.map do |n|
+    result = ''
+    result += 'foo' if (n % 3).zero?
+    result += 'bar' if (n % 5).zero?
+    result += n.to_s if result.empty?
+    result
+  end.join(' ')
+end
